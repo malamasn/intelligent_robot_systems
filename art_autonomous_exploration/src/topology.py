05f8a9ec8bbac4faf72830e2d9a73a3c31568e05
Nikolas Malamas <malamasn@hotmail.com>
2019-02-01 00:57:46 +0200

    Extra challenge 2 - part 3 - accelerate topology calculations

--- art_autonomous_exploration/src/topology.py
+++ art_autonomous_exploration/src/topology.py
@@ -19,24 +19,25 @@ from skimage.morphology import skeletonize
 class Topology:
 
   def skeletonizationCffi(self, ogm, origin, resolution, ogml):
-    width = ogm.shape[0]
-    height = ogm.shape[1]
+    # width = ogm.shape[0]
+    # height = ogm.shape[1]
 
     local = numpy.zeros(ogm.shape)
+    local[ogm < 49] = 1
+    # for i in range(0, width):
+    #   for j in range(0, height):
+    #     if ogm[i][j] < 49:
+    #       local[i][j] = 1
 
-    for i in range(0, width):
-      for j in range(0, height):
-        if ogm[i][j] < 49:
-          local[i][j] = 1
-    
     skeleton = Cffi.thinning(local, ogml)
     skeleton = Cffi.prune(skeleton, ogml, 10)
-  
-    viz = []
-    for i in range(0, width):
-      for j in range(0, height):
-        if skeleton[i][j] == 1:
-          viz.append([i * resolution + origin['x'],j * resolution + origin['y']])
+
+    # viz = []
+    # for i in range(0, width):
+    #   for j in range(0, height):
+    #     if skeleton[i][j] == 1:
+    #       viz.append([i * resolution + origin['x'],j * resolution + origin['y']])
+    viz = (numpy.array(numpy.where(skeleton == 1)).T * resolution + [origin['x'], origin['y']]).tolist()
 
     RvizHandler.printMarker(\
             viz,\
@@ -51,33 +52,33 @@ class Topology:
     return skeleton
 
   def skeletonization(self, ogm, origin, resolution, ogml):
-    width = ogm.shape[0]
-    height = ogm.shape[1]
+    # width = ogm.shape[0]
+    # height = ogm.shape[1]
 
     useful_ogm = ogm[ ogml['min_x']:ogml['max_x'] , ogml['min_y']:ogml['max_y'] ]
-    useful_width = useful_ogm.shape[0]
-    useful_height = useful_ogm.shape[1]
+    # useful_width = useful_ogm.shape[0]
+    # useful_height = useful_ogm.shape[1]
 
-    local = numpy.zeros(ogm.shape)
+    # local = numpy.zeros(ogm.shape)
     useful_local = numpy.zeros(useful_ogm.shape)
+    useful_local[useful_ogm < 49] = 1
+    # for i in range(0, useful_width):
+    #   for j in range(0, useful_height):
+    #     if useful_ogm[i][j] < 49:
+    #       useful_local[i][j] = 1
 
-    for i in range(0, useful_width):
-      for j in range(0, useful_height):
-        if useful_ogm[i][j] < 49:
-          useful_local[i][j] = 1
-      
     skeleton = skeletonize(useful_local)
     skeleton = self.pruning(skeleton, 10)
-  
+
     # padding
     skeleton_final = numpy.zeros(ogm.shape)
     skeleton_final[ ogml['min_x']:ogml['max_x'] , ogml['min_y']:ogml['max_y'] ] = skeleton
-
-    viz = []
-    for i in range(0, width):
-      for j in range(0, height):
-        if skeleton_final[i][j] == 1:
-          viz.append([i * resolution + origin['x'],j * resolution + origin['y']])
+    viz = (numpy.array(numpy.where(skeleton_final == 1)).T * resolution + [origin['x'], origin['y']]).tolist()
+    # viz = []
+    # for i in range(0, width):
+    #   for j in range(0, height):
+    #     if skeleton_final[i][j] == 1:
+    #       viz.append([i * resolution + origin['x'],j * resolution + origin['y']])
 
     RvizHandler.printMarker(\
             viz,\
@@ -95,38 +96,60 @@ class Topology:
 
   def topologicalNodes(self, ogm, skeleton, coverage, origin, resolution, brush, ogm_limits):
     nodes = []
-    
+
     width = ogm.shape[0]
     height = ogm.shape[1]
 
-    for i in range(1, width - 1):
-      for j in range(1, height - 1):
-        if ogm[i][j] <= 49 and brush[i][j] > 3 and \
-            skeleton[i][j] == 1 and coverage[i][j] != 100:
-          c = 0
-          for ii in range(-1, 2):
-            for jj in range(-1, 2):
-              c = c + skeleton[i + ii][j + jj]
-          
-          if (c == 2 or c == 4): # and coverage etc
-            nodes.append([i, j])
+    # for i in range(1, width - 1):
+    #   for j in range(1, height - 1):
+    #     if ogm[i][j] <= 49 and brush[i][j] > 3 and \
+    #         skeleton[i][j] == 1 and coverage[i][j] != 100:
+    #       c = 0
+    #       for ii in range(-1, 2):
+    #         for jj in range(-1, 2):
+    #           c = c + skeleton[i + ii][j + jj]
+    #
+    #       if (c == 2 or c == 4): # and coverage etc
+    #         nodes.append([i, j])
+
+    idx = numpy.where((ogm[1:width - 1, 1: height - 1] <= 49) & \
+                        (brush[1:width - 1, 1: height - 1] > 3) & \
+                        (skeleton[1:width - 1, 1: height - 1] == 1) & \
+                        (coverage[1:width - 1, 1: height - 1] != 100))
+
+    for i in range(len(idx[0])):
+        c = numpy.sum(skeleton[idx[0][i]:idx[0][i] + 3, idx[1][i]:idx[1][i] + 3])
+        if (c == 2 or c == 4):
+            nodes.append([idx[0][i] + 1, idx[1][i] + 1])
+
+
 
     # minimize number of nodes by erasing
-    change = True
-    while change:
-      change = False
-      for i in range(0, len(nodes)):
-        for j in range(0, len(nodes)):
-          if i == j:
-            continue
-          n1 = nodes[i]
-          n2 = nodes[j]
-          if math.pow(n1[0] - n2[0], 2) + math.pow(n1[1] - n2[1], 2) < 25:
-            change = True
-            del nodes[i]
-            break
-        if change:
-          break
+    # change = True
+    # while change:
+    #   change = False
+    #   for i in range(0, len(nodes)):
+    #     for j in range(0, len(nodes)):
+    #       if i == j:
+    #         continue
+    #       n1 = nodes[i]
+    #       n2 = nodes[j]
+    #       if math.pow(n1[0] - n2[0], 2) + math.pow(n1[1] - n2[1], 2) < 25:
+    #         change = True
+    #         del nodes[i]
+    #         break
+    #     if change:
+    #       break
+
+    for i in range(len(nodes) - 2, -1, -1):
+        n1 = nodes[i]
+        j = i + 1
+        while j < len(nodes):
+            n2 = nodes[j]
+            if math.pow(n1[0] - n2[0], 2) + math.pow(n1[1] - n2[1], 2) < 25:
+                del nodes[i]
+            else:
+                j += 1
 
     return nodes
 
@@ -144,4 +167,3 @@ class Topology:
               tmp_img[i][j] = 0
       img = numpy.copy(tmp_img)
     return img
-
